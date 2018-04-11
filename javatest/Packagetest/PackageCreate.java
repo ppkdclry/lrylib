456074e2f395de6de4492cfac8677159d6c68173
lry <rongyifj@126.com>
2018-04-11 16:14:06 +0800

    java创建包和使用包测试

---
+++ javatest/Packagetest/PackageCreate.java
@@ -0,0 +1,17 @@
+package cn.edu.seu.PackageTest;
+
+/**
+* Description:创建包测试
+* ProgramName:创建一个示例包
+* @author Rongyi 641093277@qq.com
+* @version 1.0
+*/
+public class PackageCreate{
+	
+	/**
+	* 输出信息
+	*/
+	public void version(){
+		System.out.println("TClass version 1.0 .");
+	}
+}
---
+++ javatest/Packagetest/PackageTest.java
@@ -0,0 +1,16 @@
+import cn.edu.seu.PackageTest.PackageCreate;
+
+/**
+* Description:使用创建包测试
+* ProgramName:包使用
+* @author Rongyi
+* @version 1.0
+*/
+public class PackageTest{
+	
+	public static void main(String[] args)
+	{
+		PackageCreate t = new PackageCreate();
+		t.version();
+	}
+}
